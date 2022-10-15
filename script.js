de3306062ba175f7bacb772b6b6559f8b8e6cdcb
Sam Carpenter <scarpenter7@outlook.com>
2022-10-15 21:31:20 +0000

    we did it

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/*
+border-radius: 15px;
+background-color: #ffe6db;
+border: 2px solid black;
+height: 60px;
+width: 200px;
+padding-top: 30px;
+font-size: 30px;
+display: block;
+*/
+
+document.getElementById("getImage").style.width = "500px";
+document.getElementById("getImage").style.height = "80px";
+document.getElementById("getImage").style.borderRadius = "15px";
+document.getElementById("getImage").style.border = "2px solid black";
+document.getElementById("getImage").style.backgroundColor = "#ffe6db";
+document.getElementById("getImage").style.fontSize = "30px";
+document.getElementById("getImage").style.display = "block";
+
 document.getElementById("getImage").addEventListener("click", function(event) {
     event.preventDefault();
     console.log("get image has been clicked!");
